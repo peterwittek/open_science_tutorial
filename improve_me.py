a4c323495ab4a378b7d8c290e3f2fd8b8a97360e
Arnau Riera <arnauriera@gmail.com>
2015-06-04 15:58:17 +0200

    vector of random numbers improved

--- improve_me.py
+++ improve_me.py
@@ -1,13 +1,11 @@
 # The header is missing and there are unnecessary imports
-import random, getopt, sys, os
+import random #getopt, sys, os
 
 # This line will only work in Python 2
-print "This script is not perfect"
+print("This script is not perfect")
 
 # The following loop could be written in a single line with list comprehension
-random_numbers = []
-for i in range(10):
-    random_numbers.append(random.random())
+random_numbers = [random.random() for i in range(10)]
 
 # Don't do this. Just don't.
 for i in range(len(random_numbers)):
