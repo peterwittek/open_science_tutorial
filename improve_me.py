578c84dbf65b0f0bc7e0209102e225a7da57cf13
Peter Wittek <peterwittek@users.noreply.github.com>
2015-06-04 10:31:43 +0200

    Links added

--- improve_me.py
+++ improve_me.py
@@ -1,10 +1,10 @@
-import random, getopt, sys, os
 # The header is missing and there are unnecessary imports
+import random, getopt, sys, os
 
-# This line will only pass in Python 2
+# This line will only work in Python 2
 print "This script is not perfect"
 
-# The following loop could be written in a single line
+# The following loop could be written in a single line with list comprehension
 random_numbers = []
 for i in range(10):
     random_numbers.append(random.random())
